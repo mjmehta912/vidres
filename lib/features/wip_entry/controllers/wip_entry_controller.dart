9ca37b4df3883e901b16e88f2e8a86fda7b9b548
mjmehta912 <monilmehta912@gmail.com>
2025-03-07 11:19:14 +0530

    added app logo and changed name

--- lib/features/wip_entry/controllers/wip_entry_controller.dart
+++ lib/features/wip_entry/controllers/wip_entry_controller.dart
@@ -259,12 +259,18 @@ class WipEntryController extends GetxController {
           iCode: selectedItemCode.value,
         );
 
-        if (response['Stock'] != null) {
+        if (response['Stock'] != null && response['Stock'] > 0) {
           SoundHelper.playSuccessSound();
           cards.add(
             {
               'CardNo': scannedData,
             },
+          );
+        } else {
+          SoundHelper.playFailureSound();
+          showAlertSnackbar(
+            'Not Available',
+            'Item not available in the godown',
           );
         }
       }
